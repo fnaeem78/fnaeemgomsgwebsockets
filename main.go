e99b2fa207b4f36524a0c970369798790fbf2f2a
Faisal Naeem <faisal.naeem@intelligent-waves.com>
2022-08-14 20:46:53 -0500

    getting a list of subscribed topics echoed back

--- main.go
+++ main.go
@@ -6,9 +6,50 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "starting server")
+	var subscribedTopics []string
+	http.HandleFunc("/", writeResponse)
+	http.HandleFunc("/subscribe", func(writer http.ResponseWriter, request *http.Request) {
+		subscribedTopics = subscribeTopic(writer, request, &subscribedTopics)
 	})
 
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func subscribeTopic(writer http.ResponseWriter, request *http.Request, subscribedTopics *[]string) []string {
+	query := request.URL.Query()
+	topic, present := query["topic"]
+	currentTopic := topic[0]
+	if !present || len(topic) == 0 {
+		writer.WriteHeader(http.StatusBadRequest)
+		return *subscribedTopics
+	} else if contains(subscribedTopics, currentTopic) {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte("topic exists"))
+		return *subscribedTopics
+	} else {
+		subscribedTopics := append(*subscribedTopics, currentTopic)
+		var topic string
+		for i := range subscribedTopics {
+			topic += subscribedTopics[i]
+			topic += "\n"
+		}
+
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(topic))
+
+		return subscribedTopics
+	}
+}
+func contains(topics *[]string, newtopic string) bool {
+	for _, value := range *topics {
+		if value == newtopic {
+			return true
+		}
+	}
+	return false
+}
+
+func writeResponse(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "starting server")
 }
